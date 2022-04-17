f019fb390f2dbabfd6b9aa6e1876c514310dc68b
santi-herrera <santiagotherreras@gmail.com>
2022-04-16 22:19:43 -0300

    cambios

--- controllers/controladorUsers.js
+++ controllers/controladorUsers.js
@@ -7,15 +7,21 @@ const controladorUsers = {
         })
     },
     register: function(req, res){
-        return res.render('register')
+        return res.render('register',{
+            productos: database.productos,
+            usuarios: database.usuario,
+        })
     },
     edit:  function(req, res){
-        return res.render('login',{
+        return res.render('profile-edit',{
             usuario:database.usuario,
         })
     },
     login:   function(req, res){
-        return res.render('login')
+        return res.render('login',{
+            productos: database.productos,
+            usuarios: database.usuario,
+        })
     },
 }
 module.exports = controladorUsers;
