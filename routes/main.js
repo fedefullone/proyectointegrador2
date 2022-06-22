1e7c7cc82994b8ec738073ccf7001d75100b8e03
unknown <bmartinezbaeza@udesa.edu.ar>
2022-06-22 19:54:27 -0300

    cambios en resultados de busqueda

--- routes/main.js
+++ routes/main.js
@@ -7,7 +7,7 @@ const  router = express.Router();
 
 /* GET home page. */
 router.get('/', controladorMain.index)
-//router.get('/search-results', controladorMain.searchResults)
+router.get('/search-results', controladorMain.searchResults)
 
 /*router.get('/product-add', controladorMain.add)*/
 
